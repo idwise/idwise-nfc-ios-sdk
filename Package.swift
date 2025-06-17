3c84b1a688285a2a9a19fbce09439eb942ab02ef
hafiz-idwise <95531001+HafizAhsan1@users.noreply.github.com>
2025-06-17 18:57:35 +0500

    Update Package.swift checksum

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let checksumForShield = "28ed6c3a69c62e19fc508176cc4401e6e282a941b0110dfed85b528e39e4a4c3"
 let checksumForFP = "cdceb6f13d2302bfdbf95f26d013264c678058ffbf1226034aaa0be1763d796b"
-let checksumForIDWiseNFC = "8d48a11867a3944a3716e4fda74e8c37652fd0aed7738b7eabb4396d7fa794c6"
+let checksumForIDWiseNFC = "06ed34a13d28bd5cd15216a3ad0a763d9c7fed1906d5dfc551e42f20f45aae57"
 
 let shieldVersion = "1-5-49"
 let fpVersion = "2.7.0"
