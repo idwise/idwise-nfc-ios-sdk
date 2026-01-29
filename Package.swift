cc2432c2194f84e039c207e00e60e96a486ad310
Waqar Zahour <waqar.zahour@idwise.com>
2026-01-29 10:59:38 +0500

    Bump up IDWiseNFCSDK version to 6.1.4

--- Package.swift
+++ Package.swift
@@ -5,11 +5,11 @@ import PackageDescription
 
 let checksumForShield = "3f80bef16ebda425b53a8e9f2e7d4a3ce9b027d8dbcccd057605acca8c3bef83"
 let checksumForFP = "9a9ac84b28395848247b41ce632fd00cbd309c9a99772a9ff0cf2099ef636937"
-let checksumForIDWiseNFC = "c578a9c002d2fa045a5dde8ab034909510e67854f9952b800d382812ecd814ea"
+let checksumForIDWiseNFC = "7615b447860b026d98f5a70ec23db85e8ace84475af8f82d9a9fce8953d7c41f"
 
 let shieldVersion = "1-5-52"
 let fpVersion = "2.11.0"
-let idwiseNFCSDKVersion = "6.1.3"
+let idwiseNFCSDKVersion = "6.1.4"
 
 let package = Package(
     name: "IDWiseNFC",
